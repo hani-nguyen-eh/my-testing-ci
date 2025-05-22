fcb1376946c5170c1a4a29f23ad7d1b77481964b
Hanh Nguyen Hong <hanh.nguyen@employmenthero.com>
2025-05-23 00:20:35 +0700

    rollback2

--- .github/workflows/scripts/shared/handle-split-workflow.js
+++ .github/workflows/scripts/shared/handle-split-workflow.js
@@ -1,7 +1,9 @@
-module.exports = ({ workflows }) => {
-  if (!workflows) {
-    return [];
-  }
+module.exports = {
+  splitWorkflows: ({ workflows }) => {
+    if (!workflows) {
+      return [];
+    }
 
-  return workflows.split(",").map((workflow) => workflow.trim());
+    return workflows.split(",").map((workflow) => workflow.trim());
+  },
 };
