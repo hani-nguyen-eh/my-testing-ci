bd957f3f5af5d583260e17dadc32adc9220d340b
Hanh Nguyen Hong <hanh.nguyen@employmenthero.com>
2025-05-26 22:17:10 +0700

    fix func params

--- .github/workflows/scripts/helpers/getEnvIdForWorkflow.js
+++ .github/workflows/scripts/helpers/getEnvIdForWorkflow.js
@@ -1,6 +1,6 @@
 // helper to get the environment ID for a workflow
 module.exports = {
-  getEnvIdForWorkflow: ({ workflow }) => {
+  getEnvIdForWorkflow: ({ workflow, environmentMappings, environmentIds }) => {
     const envName = environmentMappings[workflow];
     if (!envName) {
       console.log(`No environment mapping found for workflow: ${workflow}`);
